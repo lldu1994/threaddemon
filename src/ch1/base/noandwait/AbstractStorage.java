34b2f3b28fa6e52c99b64cda2733fa31931d34e5
duliangliang <lldu@zhishu.com>
2020-07-23 18:01:41 +0800

    更新文件：20200723

---
+++ src/ch1/base/noandwait/AbstractStorage.java
@@ -0,0 +1,6 @@
+package ch1.base.noandwait;
+
+public interface AbstractStorage {
+    void consume(int num);
+     void produce(int num);
+}
--- src/ch1/base/wn/TestWn.java
+++ src/ch1/base/wn/TestWn.java
@@ -4,5 +4,5 @@ package ch1.base.wn;
  *  测试wait/notify/notifyAll
  */
 public class TestWn {
-    private  static  Express express =
+    private  static  Express express =null;
 }
